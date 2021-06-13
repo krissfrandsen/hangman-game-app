b78037da6e2cd62403e9bade9b1e0363d377870b
Kriss <krissfrandsen@gmail.com>
2021-06-14 00:21:11 +0200

    refactor notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -10,46 +10,34 @@ type GameType = {
 const Notification: React.FC<GameType> = ({ restartGame }) => {
   const { count, randomNumber, guessStatus, isEndGame } =
     useContext(HangmanContext);
-  console.log(guessStatus.indexOf("_") < 0);
+
+  let finalMessage = "";
+  let finalMessageRevealWord = "";
+  let finalMessageWordMeaning = "";
+
+  if (isEndGame && guessStatus.indexOf("_") < 0) {
+    finalMessage = "Congratulations! You won! 😃";
+  } else if (isEndGame && guessStatus.indexOf("_") >= 0 && count >= 10) {
+    finalMessage = "Unfortunately you lost. 😕";
+    finalMessageRevealWord = `The right word was: ${wordList[randomNumber].term}`;
+    finalMessageWordMeaning = `${wordList[randomNumber].meaning}`;
+  }
 
   return (
     <div className="notification--wrapper">
-      {isEndGame && guessStatus.indexOf("_") < 0 ? (
-        <>
-          <h2>Congratulations!</h2>
-          <Button
-            className="button btn--play btn__correct"
-            onClick={() => restartGame()}
-            children="Play Again"
-          />
-        </>
-      ) : (
-        ""
-      )}
-      {isEndGame && guessStatus.indexOf("_") >= 0 && count >= 10 ? (
-        <>
-          <h2>You lose!</h2>
-          <p>
-            Correct word is:
-            <span className="notification--wrapper__item">
-              {wordList[randomNumber].term}
-            </span>
-          </p>
-          <p>
-            Meaning:
-            <span className="notification--wrapper__item">
-              {wordList[randomNumber].meaning}
-            </span>
-          </p>
-          <Button
-            className="button btn--play btn__wrong"
-            onClick={() => restartGame()}
-            children="Try Again"
-          />
-        </>
-      ) : (
-        ""
-      )}
+      <h2>{finalMessage}</h2>
+      <p>{finalMessageRevealWord}</p>
+      <p>
+        Meaning:
+        <span className="notification--wrapper__item">
+          {finalMessageWordMeaning}
+        </span>
+      </p>
+      <Button
+        className="button btn--play"
+        onClick={() => restartGame()}
+        children="Play Again"
+      />
     </div>
   );
 };
